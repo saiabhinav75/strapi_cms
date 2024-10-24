dac56a2ae04c4a04141039065de0fcced1ec894a
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2024-10-24 15:49:18 +0530

    resolve all bugs for the dbs connection

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,37 @@
 const config = ({ env }: { env: (key: string) => string }) => ({
-    'strapi-plugin-populate-deep': {
-      config: {
-        defaultDepth: 3, // Customize depth
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 3, 
+    }
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials:{
+            accessKeyId: '735ca3009617cf622ebaa1ec17033683',
+            secretAccessKey: "e46c44d9aae12b0839ad92b174bf45a241cf4ae5b4b624abda27a65bcfcbcd7a",
+          },
+          endpoint:"https://zragvlgzbensfgplfuey.supabase.co/storage/v1/s3",
+          baseUrl:'https://zragvlgzbensfgplfuey.supabase.co/storage/v1/object/public',
+          rootPath:'CMS',
+          region:'ap-south-1',
+          params: {
+            Bucket: 'CMS',
+            ACL:'public-read'
+          },
+        }
+      },
+      actionOptions: {
+        upload: {
+        },
+        uploadStream: {
+        },
       }
     },
-  });
+  }
   
-  export default config;
-  
+});
+
+export default config;
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -192,18 +192,6 @@ export interface RcaPartA extends Struct.ComponentSchema {
   };
 }
 
-export interface QbComponentsMtfColAOption extends Struct.ComponentSchema {
-  collectionName: 'components_qb_components_mtf_col_a_options';
-  info: {
-    displayName: 'mtf_colA_option';
-    description: '';
-  };
-  attributes: {
-    left_option: Schema.Attribute.String;
-    right_option: Schema.Attribute.Component<'block.option', true>;
-  };
-}
-
 export interface BlockSubjective extends Struct.ComponentSchema {
   collectionName: 'components_block_subjectives';
   info: {
@@ -301,6 +289,18 @@ export interface BlockAudioQuestion extends Struct.ComponentSchema {
   };
 }
 
+export interface QbComponentsMtfColAOption extends Struct.ComponentSchema {
+  collectionName: 'components_qb_components_mtf_col_a_options';
+  info: {
+    displayName: 'mtf_colA_option';
+    description: '';
+  };
+  attributes: {
+    left_option: Schema.Attribute.String;
+    right_option: Schema.Attribute.Component<'block.option', true>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
@@ -318,7 +318,6 @@ declare module '@strapi/strapi' {
       'vocab.adverbs': VocabAdverbs;
       'rca.part-b': RcaPartB;
       'rca.part-a': RcaPartA;
-      'qb-components.mtf-col-a-option': QbComponentsMtfColAOption;
       'block.subjective': BlockSubjective;
       'block.option': BlockOption;
       'block.mtf': BlockMtf;
@@ -326,6 +325,7 @@ declare module '@strapi/strapi' {
       'block.mcq-question': BlockMcqQuestion;
       'block.fib': BlockFib;
       'block.audio-question': BlockAudioQuestion;
+      'qb-components.mtf-col-a-option': QbComponentsMtfColAOption;
     }
   }
 }
