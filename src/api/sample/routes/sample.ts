3c8530951f154aed6f8ed96f41bbe2ee6b08037a
saiabhinav75 <abhinavabhi7575@gmail.com>
2024-12-21 12:59:54 +0530

    before upgrade

---
+++ src/api/sample/routes/sample.ts
@@ -0,0 +1,7 @@
+/**
+ * sample router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sample.sample');
---
+++ src/api/sample/services/sample.ts
@@ -0,0 +1,7 @@
+/**
+ * sample service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sample.sample');
